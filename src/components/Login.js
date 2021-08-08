1804c7299c7280cf2de2378f62e90d1d54e9e5c3
RottK <r.kitchell815@gmail.com>
2021-08-07 20:06:58 -0500

    updated Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import { useState } from 'react';
+import {connect} from 'react-redux';
+import {Form, Button} from 'react-bootstrap';
+import {validateFields} from '../utils/common';
+import { Link } from 'react-router-dom'
 
-const Login = () => {
-    return <div>Login Page</div>
+function Login() {
+    const [login, setLogin] = useState({email: '',
+        password: '',
+        errorMsg: ''
+    })
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const { email, password } = login;
+        const fieldsToValidate = [{email}, {password}];
+
+        const allFieldsEntered = validateFields(fieldsToValidate);
+        if (!allFieldsEntered) {
+            setLogin({
+                errorMsg: {
+                    signin_error: 'Please enter all the fields'
+                }
+            });
+        } else {
+            setLogin({
+                errorMsg: {
+                    signin_error: ''
+                }
+            });
+        }
+    }
+
+    const handleChange = ({target}) => {
+        setLogin({
+            [target.name]: target.value
+        });
+    }
+        
+    return (
+        <div className="login-page">
+            <h1>Banking App</h1>
+            <div className="login-form">
+                <Form onSubmit={handleLogin}>
+                    {login.errorMsg && login.errorMsg.signin_error && (
+                        <p className="errorMsg centered-message">
+                            {login.errorMsg.signin_error}
+                        </p>
+                    )}
+                    <Form.Group controlId="email">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                        type="email" 
+                        name="email"
+                        placeholder="Enter email"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control 
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <div className="action-items">
+                        <Button varient="primary" type="submit">Login</Button>
+                        <Link to="/register" className="btn btn-secondary">Create account</Link>
+                    </div>
+                </Form>
+            </div>
+        </div>
+
+    )
+        
+
+    
 };
 
-export default Login;
+export default connect()(Login);
